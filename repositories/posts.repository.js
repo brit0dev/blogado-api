5d7432fef4f4f54722dda3fdef335bc00f30371e
brito <britoifpe@gmail.com>
2024-06-06 17:35:48 -0300

    Finishing the PostsRepository.

--- repositories/posts.repository.js
+++ repositories/posts.repository.js
@@ -1,18 +1,36 @@
+import { Post } from "../models/post.model.js";
+
 export default class PostsRepository {
   async list() {
-    console.log("get");
-    return { Message: "get" };
+    return await Post.find({}, { __v: false }).lean();
   }
   async findById(id) {
-    console.log("find", id);
+    const post = await Post.findById(id, { __v: false }).lean();
+
+    if (post == null) throw { APIMessage: "Not Found", APIStatus: 404 };
+
+    return post;
   }
   async create(data) {
-    console.log("create", data);
+    return await Post.create(data);
   }
   async update(id, data) {
-    console.log("update", id, data);
+    const updatedPost = await Post.findByIdAndUpdate(id, data, {
+      lean: true,
+      projection: { __v: false },
+      new: true,
+    });
+
+    if (updatedPost == null) throw { APIMessage: "Not Found", APIStatus: 404 };
+
+    return updatedPost;
   }
   async delete(id) {
-    console.log("delete", id);
+    const deleteQuery = await Post.deleteOne({ _id: id });
+
+    if (deleteQuery.deletedCount == 0)
+      throw { APIMessage: "Not Found", APIStatus: 404 };
+
+    return true;
   }
 }
